ede43c5674f4735118756f2c8a819d69290b6690
j0hannr <johann.rohn@gmail.com>
2021-04-14 14:19:34 +0200

    player hidden mode

--- components/audio/Waveform.js
+++ components/audio/Waveform.js
@@ -39,7 +39,7 @@ export default function Waveform({ url }) {
     wavesurfer.current.on("ready", function () {
       // https://wavesurfer-js.org/docs/methods.html
       // wavesurfer.current.play();
-      setPlay(true);
+      setPlay(false);
 
       // Auto start Player:
       // handleRestart();
@@ -84,6 +84,9 @@ export default function Waveform({ url }) {
 
       <div className="controls">
 
+        {/* restart button */}
+        {/* <img width="15px" src="../icons/backward.svg" onClick={handleRestart} /> */}
+
         {/* <button onClick={handlePlayPause} >{!playing ? "Play" : "Pause"}</button> */}
 
         {/* play pause button */}
@@ -91,9 +94,6 @@ export default function Waveform({ url }) {
           <img className={!playing ? "active" : null} width="15px" src="../icons/play.svg" />
           <img className={!playing ? null : "active"} width="15px" src="../icons/pause.svg" />
         </div>
-
-        {/* restart button */}
-        {/* <img width="15px" src="../icons/backward.svg" onClick={handleRestart} /> */}
 
       </div>
     </div>
